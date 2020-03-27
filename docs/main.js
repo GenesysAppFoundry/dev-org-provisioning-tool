ba283394f3122b320e8bf919e8c2008f3655241a
AgnesCorpuz <38449514+AgnesCorpuz@users.noreply.github.com>
2020-03-27 12:04:03 +0800

    Update styles

--- docs/main.js
+++ docs/main.js
@@ -5,16 +5,24 @@ $(document).ready(() => {
     // Load provisioning tool in iframe on page load
     document.getElementById('tool-iframe').src = '/org-provisioning/index.html';
 
+    // Set initial style
+    document.getElementById("btnProvisioning").style.fontWeight = 'bold';
+    document.getElementById("btnListing").style.fontWeight = 'normal';
+
     // Click event to load provisioning tool in iframe
     document.getElementById("btnProvisioning").addEventListener('click', function() {
         document.getElementById('tool-iframe').src = '/org-provisioning/index.html';
         document.documentElement.scrollTop = 450;
+        document.getElementById("btnProvisioning").style.fontWeight = 'bold';
+        document.getElementById("btnListing").style.fontWeight = 'normal';
     })
 
     // Click evet to load listing management in iframe
     document.getElementById("btnListing").addEventListener('click', function() {
         document.getElementById('tool-iframe').src = '/listing-management/partner-side/index.html'
         document.documentElement.scrollTop = 450;
+        document.getElementById("btnProvisioning").style.fontWeight = 'normal';
+        document.getElementById("btnListing").style.fontWeight = 'bold';
     })
 })
 
