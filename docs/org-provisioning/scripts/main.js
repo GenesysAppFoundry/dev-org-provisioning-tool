b57a5672bebe4b8d451392e62569dcf1a6450838
jenissabarrera <Jenissa.Barrera@genesys.com>
2020-03-31 10:27:54 +0800

    authentication for dev org provisioning part

--- docs/org-provisioning/scripts/main.js
+++ docs/org-provisioning/scripts/main.js
@@ -2,16 +2,24 @@ import provisionTelephonyViews from './views/provisiontelephony.js'
 import architectFlowViews from './views/architectflow.js'
 import loadModalView from './components/modals.js'
 import developerRoleFlowViews from './views/developerRole.js'
+import globalConfig from '../../config/global-config.js'
 
-const clientId = '83d37bf5-e050-47bf-9937-0314b259c9c4';
+// const clientId = '83d37bf5-e050-47bf-9937-0314b259c9c4';
 const redirectUri = window.location.href;
 const platformClient = require('platformClient');
 const client = platformClient.ApiClient.instance;
 
-// Set PureCloud settings
-client.setEnvironment('mypurecloud.com');
+let environment = '';
+
+//Authenticate
+environment = localStorage.getItem(globalConfig.appName + ':environment');
+if(!environment){
+    throw new Error('Environment not found from localstorage.');
+}
+let clientId = globalConfig.clientIDs[environment]; 
+client.setEnvironment(environment);
 $(document).ready(() => {
-     client.loginImplicitGrant(clientId, redirectUri)
+    client.loginImplicitGrant(clientId, window.location.href.split('?')[0])
          .then(() => {
              console.log('Logged in');
             let token = client.authData.accessToken;
