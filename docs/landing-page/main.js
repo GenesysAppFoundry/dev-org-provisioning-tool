2fd3d45dcc7eb06212ee68e9826793b9fcc42630
AgnesCorpuz <38449514+AgnesCorpuz@users.noreply.github.com>
2020-04-07 12:17:17 +0800

    Add root config

--- docs/landing-page/main.js
+++ docs/landing-page/main.js
@@ -8,9 +8,6 @@ $(document).ready(() => {
 
     document.body.appendChild(newHeaderEl);
     document.body.appendChild(newMainMenuEl);
-    
-    // Trigger uninstall button to uninstall app.
-    document.getElementById("btnUninstall").addEventListener('click', () => window.location='./wizard/uninstall.html');
 })
 
 
